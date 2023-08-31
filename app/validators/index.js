76d97b427ac0960a8c8cb68e2de85ccd7c95aae0
DannStormy <dillydot22@gmail.com>
2023-08-31 22:36:28 +0100

    chore: updated validation

--- app/validators/index.js
+++ app/validators/index.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const addAccountSchema = Joi.object({
   name: Joi.string().required(),
-  dob: Joi.string().required(),
+  dob: Joi.date().max('now').required(),
   account_type: Joi.string().valid('savings', 'checking').required(),
   initial_balance: Joi.number().positive().required()
 });
