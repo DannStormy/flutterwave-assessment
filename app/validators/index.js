2fc430ff7ca4d02ba78ac1bc5299e70974f97ae7
DannStormy <dillydot22@gmail.com>
2023-09-01 09:40:20 +0100

    fix: joi validation

--- app/validators/index.js
+++ app/validators/index.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const addAccountSchema = Joi.object({
   name: Joi.string().required(),
-  dob: Joi.date().max('now').required(),
-  account_type: Joi.string().valid('savings', 'checking').required(),
+  dob: Joi.string().required(),
+  account_type: Joi.string().valid('savings', 'checking').required(), //more account types if needed
   initial_balance: Joi.number().positive().required()
 });
